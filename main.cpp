c87e53506980261f0f73b078e4a1148573d24b44
ljianhui <ljianhui2012@163.com>
2015-07-04 01:09:47 +0800

    Add test code and use -O3 optimize

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
-#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
 #include "memorypoolfixed.h"
+
+using std::cout;
+using std::endl;
+
+double CurrentTime()
+{
+	struct timeval timeVal;
+	struct timezone timeZone;
+	gettimeofday(&timeVal, &timeZone);
+	double curTime  = (double)timeVal.tv_sec + 
+		(double)timeVal.tv_usec / (double)1e6;
+	return curTime;
+}
+
+typedef double DataType;
 
 int main()
 {
-	MemoryPoolFixed pool(sizeof(int), 100);
-	int *p1 = (int*)pool.alloc();
-	*p1 = 1;
-	int *p2 = (int*)pool.alloc();
-	*p2 = 2;
-	int *p3 = (int*)pool.alloc();
-	*p3 = 3;
-	printf("%d, %d, %d\n", *p1, *p2, *p3);
-	printf("used: %lu, free: %lu\n", pool.getUsedUnitCount(), pool.getFreeUnitCount());
-	pool.free(p2);
-	printf("used: %lu, free: %lu\n", pool.getUsedUnitCount(), pool.getFreeUnitCount());
+	double time = 0;
+	{
+	MemoryPoolFixed pool(sizeof(DataType), 100*1024*1024);
+	time = CurrentTime();
+	for (int i = 0; i < 100 * 1024 * 1024; ++i)
+	{
+		DataType *p = (DataType*)pool.alloc();
+		*p = i;
+		pool.free(p);
+	}
+	cout << CurrentTime() - time << endl;
+	}
+
+	time = CurrentTime();
+	for (int i = 0; i < 100 * 1024 * 1024; ++i)
+	{
+		DataType *p = new DataType;
+		*p = i;
+		delete p;
+	}
+	cout << CurrentTime() - time << endl;
+
+	time = CurrentTime();
+	for (int i = 0; i < 100 * 1024 * 1024; ++i)
+	{
+		DataType *p = (DataType*)malloc(sizeof(DataType));
+		*p = i;
+		free(p);
+	}
+	cout << CurrentTime() - time << endl;
 	return 0;
 }
 
--- makefile
+++ makefile
@@ -3,16 +3,17 @@ APPNAME := memorypooltest
 all: $(APPNAME)
 
 CC := g++
+CFLAG := -O3
 
 OBJS := main.o memorypoolfixed.o
 
 $(APPNAME): $(OBJS)
-	$(CC) -o $(APPNAME) $(OBJS)
+	$(CC) -o $(APPNAME) $(OBJS) $(CFLAG)
 
 main.o: main.cpp memorypoolfixed.h
-	$(CC) -c main.cpp
+	$(CC) -c main.cpp $(CFLAG)
 memorypoolfixed.o: memorypoolfixed.h memorypoolfixed.cpp
-	$(CC) -c memorypoolfixed.cpp
+	$(CC) -c memorypoolfixed.cpp $(CFLAG)
 clean:
 	rm -f *.o $(APPNAME)
 
